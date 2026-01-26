ac2f5168538ef0c243b45e92c6935a2882ddc56e
Jakucs Viktor <jakucs42@gmail.com>
2026-01-26 21:44:55 +0100

    add some fixes

--- process.js
+++ process.js
@@ -6,38 +6,38 @@ const movieDB = {
   genres: []
 }
 
+
+
+
+
 //Ha nincs a profession-be, hozzáadjuk új id-val
 
 function createProfessionalObject(movie) {
-	let idCounter;
-
-	for (let i = movieDB.professionals.length + 1; i < movieDB.professionals.length; i++) {
+	//ha i=length+1 akkor i<length nemlesz igaz
+	const idCounter = movieDB.professionals.length + 1;
+	//mert mindig létrehozunk egy új professionalObjectet
+/* 	for (let i = movieDB.professionals.length + 1; i < movieDB.professionals.length; i++) {
 		idCounter = i;
-	}
+		console.log("hello")
+	} */
 
 	const professionalObject = {
          id: idCounter,
        name: "",
 	   roles: []
-
-
 	}
 
 	const roles = [];
 
 	roles.push(movie.writers, movie.actors, movie.directors);
-
+	//console.log(roles)
+	//roles = [...movie.writers, ...movie.actors, ...movie.directors];
 
 		for (const role of roles) {
 			professionalObject.name = getName(role);
 		}
 
-
-
-
-
 	return professionalObject;
-	
 }
 
 function getName(role) {
