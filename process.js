3a96c212ea09e2238c2c8e42f087155c16f2bb83
Jakucs Viktor <jakucs42@gmail.com>
2026-01-27 17:36:37 +0100

    add makeRolesArray

--- process.js
+++ process.js
@@ -1,10 +1,58 @@
 import data from "./data.json" with { type: "json" };
 
 const movieDB = {
-	professionals: [],
+	professionals: [], //rendező színész író külön tömbbe kerül
 	movies: [],
   genres: []
 }
+
+createProfessionalObject(getAllNames(), makeRoles())
+
+function createProfessionalObject (names, roles) {
+	let idCounter = movieDB.professionals.length+1
+	let professionalObject = {
+		id: idCounter,
+		name: "",
+		roles: []
+	}
+	
+
+	//console.log(writersArray)
+	console.log(professionalObject)
+
+	movieDB.professionals.push(professionalObject)
+}
+
+
+function getAllNames(){
+	let names=[];
+	for(let movie of data.movies){
+
+		for(let name of movie.writers){
+			if(!names.includes(name)){
+				names.push(name)
+			}
+		}
+		
+		for(let name of movie.actors){
+			if(!names.includes(name)){
+				names.push(name)
+			}
+		}
+		
+		for(let name of movie.directors){
+			if(!names.includes(name)){
+				names.push(name)
+			}
+		}
+		
+	}
+	console.log(names)
+	return names
+}
+
+
+
 
 function makeRoles(){
 	let professionals = {
@@ -50,8 +98,40 @@ function makeRoles(){
 	return professionals
 }
 
-console.log(makeRoles())
 
 
+
+/* function makeRolesArray(){
+		//Write tömböt létrehozzuk, csak a writerek lesznek benne.
+	let writersArray = [];
+	for(let writers of roles.writers){
+		for(let writer of writers){
+			if(!writersArray.includes(writer)){
+				writersArray.push(writer)
+			}
+			professionalObject.roles.push(writersArray)
+		}
+	}
+	//Actors tömböt létrehozzuk, csak a writerek lesznek benne.
+	let actorsArray = [];
+	for(let actors of roles.actors){
+		for(let actor of actors){
+			if(!actorsArray.includes(actor)){
+			actorsArray.push(actor)
+			}
+			professionalObject.roles.push(actorsArray)
+		}
+	}
+	//Director tömböt létrehozzuk, csak a writerek lesznek benne.
+	let directorsArray = [];
+	for(let directors of roles.directors){
+		for(let director of directors){
+			if(!directorsArray.includes(director)){
+			directorsArray.push(director)
+			}
+			professionalObject.roles.push(directorsArray)
+		}
+	}
+} */
 
 export {movieDB};
