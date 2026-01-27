e2630d24b6d0491fa25dad1a9d59930532715838
Jakucs Viktor <jakucs42@gmail.com>
2026-01-27 22:19:39 +0100

    add actors to site

--- client/public/script.js
+++ client/public/script.js
@@ -8,16 +8,26 @@ const loadEvent = function() {
   console.log("page: ", page);
   
   const rootElement = document.getElementById("root");
-  if (page = "movies") {
-    
-  }else if(page = "actors"){
+  if (page == "movies") {
+    let root = document.getElementById("root")
+    let ul = document.createElement("ul")
+    ul.textContent="Actors:"
+    for(let name of data.professionals){
+      let li = document.createElement("li")
+      li.textContent=name.name
+      ul.appendChild(li)
+    }
+    root.appendChild(ul)
 
-  }else if(page = "directors"){
+  }else if(page == "actors"){
 
-  }else if(page = "writers"){
+  }else if(page == "directors"){
+
+  }else if(page == "writers"){
     
   }
   
+
 
 }
 
